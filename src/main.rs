17198f05f1d17c7ceef0a975aba836a61cb84229
Shota Tsukamoto <shootacean@gmail.com>
2020-11-02 22:09:25 +0900

    fizzbuzz

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,23 @@
 fn main() {
+    hello();
+    fizzbuzz();
+}
+
+fn hello() {
     let name = "Hello world";
     println!("{}", name);
+}
+
+fn fizzbuzz() {
+    for n in 1..=100 {
+        if n % 15 == 0 {
+            println!("fizzbuzz");
+        } else if n % 3 == 0 {
+            println!("fizz");
+        } else if n % 5 == 0 {
+            println!("buzz");
+        } else {
+            println!("{}", n);
+        }
+    }
 }
